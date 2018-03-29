ab28105b0b19b7a63fbde861375fd0ca520f6635
Tony <Naplex@Tonys-MacBook.local>
2018-03-29 15:21:53 -0700

    questions and answers are all working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 // create objects with questions and answers.
 
 var Q1 = {
-    question : "Which was the first professional fighter to beat Mike Tyson?",
+    question : "Who was the first professional fighter to beat Mike Tyson?",
     choices : ["Mitch Blood Green", "Razor Ruddock", "Evander Holyfield", "Buster Douglas"],
     correctAn : "Buster Douglas"
 };
@@ -85,16 +85,198 @@ var Q16 = {
 var questions = [Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12, Q13, Q14, Q15, Q16];
 var wrongCount = 0;
 var correctCount = 0;
+var unaswered = 0;
+var i = 1;
+console.log(i);
+console.log(Q1.correctAn);
 // create a function to display intro, correct, and wrong divs.
 function loadQuestion(){
-    for (var i =0 ; i < questions.length; i++){
-        document.getElementById()
+    if (i === 1){
+        document.getElementById("currQuestion").innerHTML = Q1.question;
+        document.getElementById("option1").innerHTML = Q1.choices[0];
+        document.getElementById("option2").innerHTML = Q1.choices[1];
+        document.getElementById("option3").innerHTML = Q1.choices[2];
+        document.getElementById("option4").innerHTML = Q1.choices[3];
+        console.log(Q1.correctAn);
+    }
+    else if (i === 2){
+        document.getElementById("currQuestion").innerHTML = Q2.question;
+        document.getElementById("option1").innerHTML = Q2.choices[0];
+        document.getElementById("option2").innerHTML = Q2.choices[1];
+        document.getElementById("option3").innerHTML = Q2.choices[2];
+        document.getElementById("option4").innerHTML = Q2.choices[3];
+        console.log(Q2.correctAn);
+    }
+    else if (i === 3){
+        document.getElementById("currQuestion").innerHTML = Q3.question;
+        document.getElementById("option1").innerHTML = Q3.choices[0];
+        document.getElementById("option2").innerHTML = Q3.choices[1];
+        document.getElementById("option3").innerHTML = Q3.choices[2];
+        document.getElementById("option4").innerHTML = Q3.choices[3];
+        console.log(Q3.correctAn);
+    }
+    else if (i === 4){
+        document.getElementById("currQuestion").innerHTML = Q4.question;
+        document.getElementById("option1").innerHTML = Q4.choices[0];
+        document.getElementById("option2").innerHTML = Q4.choices[1];
+        document.getElementById("option3").innerHTML = Q4.choices[2];
+        document.getElementById("option4").innerHTML = Q4.choices[3];
+        console.log(Q4.correctAn);
+    }
+    else if (i === 5){
+        document.getElementById("currQuestion").innerHTML = Q5.question;
+        document.getElementById("option1").innerHTML = Q5.choices[0];
+        document.getElementById("option2").innerHTML = Q5.choices[1];
+        document.getElementById("option3").innerHTML = Q5.choices[2];
+        document.getElementById("option4").innerHTML = Q5.choices[3];
+        console.log(Q5.correctAn);
+    }
+    else if (i === 6){
+        document.getElementById("currQuestion").innerHTML = Q6.question;
+        document.getElementById("option1").innerHTML = Q6.choices[0];
+        document.getElementById("option2").innerHTML = Q6.choices[1];
+        document.getElementById("option3").innerHTML = Q6.choices[2];
+        document.getElementById("option4").innerHTML = Q6.choices[3];
+        console.log(Q6.correctAn);
+    }
+    else if (i === 7){
+        document.getElementById("currQuestion").innerHTML = Q7.question;
+        document.getElementById("option1").innerHTML = Q7.choices[0];
+        document.getElementById("option2").innerHTML = Q7.choices[1];
+        document.getElementById("option3").innerHTML = Q7.choices[2];
+        document.getElementById("option4").innerHTML = Q7.choices[3];
+        console.log(Q7.correctAn);
+    }
+    else if (i === 8){
+        document.getElementById("currQuestion").innerHTML = Q8.question;
+        document.getElementById("option1").innerHTML = Q8.choices[0];
+        document.getElementById("option2").innerHTML = Q8.choices[1];
+        document.getElementById("option3").innerHTML = Q8.choices[2];
+        document.getElementById("option4").innerHTML = Q8.choices[3];
+        console.log(Q8.correctAn);
+    }
+    else if (i === 9){
+        document.getElementById("currQuestion").innerHTML = Q9.question;
+        document.getElementById("option1").innerHTML = Q9.choices[0];
+        document.getElementById("option2").innerHTML = Q9.choices[1];
+        document.getElementById("option3").innerHTML = Q9.choices[2];
+        document.getElementById("option4").innerHTML = Q9.choices[3];
+        console.log(Q9.correctAn);
+    }
+    else if (i === 10){
+        document.getElementById("currQuestion").innerHTML = Q10.question;
+        document.getElementById("option1").innerHTML = Q10.choices[0];
+        document.getElementById("option2").innerHTML = Q10.choices[1];
+        document.getElementById("option3").innerHTML = Q10.choices[2];
+        document.getElementById("option4").innerHTML = Q10.choices[3];
+        console.log(Q10.correctAn);
+    }
+    else if (i === 11){
+        document.getElementById("currQuestion").innerHTML = Q11.question;
+        document.getElementById("option1").innerHTML = Q11.choices[0];
+        document.getElementById("option2").innerHTML = Q11.choices[1];
+        document.getElementById("option3").innerHTML = Q11.choices[2];
+        document.getElementById("option4").innerHTML = Q11.choices[3];
+        console.log(Q11.correctAn);
+    }
+    else if (i === 12){
+        document.getElementById("currQuestion").innerHTML = Q12.question;
+        document.getElementById("option1").innerHTML = Q12.choices[0];
+        document.getElementById("option2").innerHTML = Q12.choices[1];
+        document.getElementById("option3").innerHTML = Q12.choices[2];
+        document.getElementById("option4").innerHTML = Q12.choices[3];
+        console.log(Q12.correctAn);
+    }
+    else if (i === 13){
+        document.getElementById("currQuestion").innerHTML = Q13.question;
+        document.getElementById("option1").innerHTML = Q13.choices[0];
+        document.getElementById("option2").innerHTML = Q13.choices[1];
+        document.getElementById("option3").innerHTML = Q13.choices[2];
+        document.getElementById("option4").innerHTML = Q13.choices[3];
+        console.log(Q13.correctAn);
+    }
+    else if (i === 14){
+        document.getElementById("currQuestion").innerHTML = Q14.question;
+        document.getElementById("option1").innerHTML = Q14.choices[0];
+        document.getElementById("option2").innerHTML = Q14.choices[1];
+        document.getElementById("option3").innerHTML = Q14.choices[2];
+        document.getElementById("option4").innerHTML = Q14.choices[3];
+        console.log(Q14.correctAn);
+    }
+    else if (i === 15){
+        document.getElementById("currQuestion").innerHTML = Q15.question;
+        document.getElementById("option1").innerHTML = Q15.choices[0];
+        document.getElementById("option2").innerHTML = Q15.choices[1];
+        document.getElementById("option3").innerHTML = Q15.choices[2];
+        document.getElementById("option4").innerHTML = Q15.choices[3];
+        console.log(Q15.correctAn);
+    }
+    else if (i === 16){
+        document.getElementById("currQuestion").innerHTML = Q16.question;
+        document.getElementById("option1").innerHTML = Q16.choices[0];
+        document.getElementById("option2").innerHTML = Q16.choices[1];
+        document.getElementById("option3").innerHTML = Q16.choices[2];
+        document.getElementById("option4").innerHTML = Q16.choices[3];
+        console.log(Q16.correctAn);
     }
 }
-function answerValidation(){
+function answerValidation1(){
+    if (document.getElementById("option1").innerHTML === Q1.correctAn || document.getElementById("option1").innerHTML === Q2.correctAn || document.getElementById("option1").innerHTML === Q3.correctAn || document.getElementById("option1").innerHTML === Q4.correctAn || document.getElementById("option1").innerHTML === Q5.correctAn || document.getElementById("option1").innerHTML === Q6.correctAn || document.getElementById("option1").innerHTML === Q7.correctAn || document.getElementById("option1").innerHTML === Q8.correctAn || document.getElementById("option1").innerHTML === Q9.correctAn || document.getElementById("option1").innerHTML === Q10.correctAn || document.getElementById("option1").innerHTML === Q11.correctAn || document.getElementById("option1").innerHTML === Q12.correctAn || document.getElementById("option1").innerHTML === Q13.correctAn || document.getElementById("option1").innerHTML === Q14.correctAn || document.getElementById("option1").innerHTML === Q15.correctAn || document.getElementById("option1").innerHTML === Q16.correctAn){
+        correct();
+        i++;
+        correctCount++;
+        loadQuestion();
+    }
+    else {
+        wrong();
+        i++;
+        wrongCount++;
+        loadQuestion();
+    }
+}
+function answerValidation2(){
+    if (document.getElementById("option2").innerHTML === Q1.correctAn || document.getElementById("option2").innerHTML === Q2.correctAn || document.getElementById("option2").innerHTML === Q3.correctAn || document.getElementById("option2").innerHTML === Q4.correctAn || document.getElementById("option2").innerHTML === Q5.correctAn || document.getElementById("option2").innerHTML === Q6.correctAn || document.getElementById("option2").innerHTML === Q7.correctAn || document.getElementById("option2").innerHTML === Q8.correctAn || document.getElementById("option2").innerHTML === Q9.correctAn || document.getElementById("option2").innerHTML === Q10.correctAn || document.getElementById("option2").innerHTML === Q11.correctAn || document.getElementById("option2").innerHTML === Q12.correctAn || document.getElementById("option2").innerHTML === Q13.correctAn || document.getElementById("option2").innerHTML === Q14.correctAn || document.getElementById("option2").innerHTML === Q15.correctAn || document.getElementById("option2").innerHTML === Q16.correctAn){
+        correct();
+        i++;
+        correctCount++;
+        loadQuestion();
+    }
+    else {
+        wrong();
+        i++;
+        wrongCount++;
+        loadQuestion();
+    }
+}
+function answerValidation3(){
+    if (document.getElementById("option3").innerHTML === Q1.correctAn || document.getElementById("option3").innerHTML === Q2.correctAn || document.getElementById("option3").innerHTML === Q3.correctAn || document.getElementById("option3").innerHTML === Q4.correctAn || document.getElementById("option3").innerHTML === Q5.correctAn || document.getElementById("option3").innerHTML === Q6.correctAn || document.getElementById("option3").innerHTML === Q7.correctAn || document.getElementById("option3").innerHTML === Q8.correctAn || document.getElementById("option3").innerHTML === Q9.correctAn || document.getElementById("option3").innerHTML === Q10.correctAn || document.getElementById("option3").innerHTML === Q11.correctAn || document.getElementById("option3").innerHTML === Q12.correctAn || document.getElementById("option3").innerHTML === Q13.correctAn || document.getElementById("option3").innerHTML === Q14.correctAn || document.getElementById("option3").innerHTML === Q15.correctAn || document.getElementById("option3").innerHTML === Q16.correctAn){
+        correct();
+        i++;
+        correctCount++;
+        loadQuestion();
+    }
+    else {
+        wrong();
+        i++;
+        wrongCount++;
+        loadQuestion();
+    }
+}
+function answerValidation4(){
+    if (document.getElementById("option4").innerHTML === Q1.correctAn || document.getElementById("option4").innerHTML === Q2.correctAn || document.getElementById("option4").innerHTML === Q3.correctAn || document.getElementById("option4").innerHTML === Q4.correctAn || document.getElementById("option4").innerHTML === Q5.correctAn || document.getElementById("option4").innerHTML === Q6.correctAn || document.getElementById("option4").innerHTML === Q7.correctAn || document.getElementById("option4").innerHTML === Q8.correctAn || document.getElementById("option4").innerHTML === Q9.correctAn || document.getElementById("option4").innerHTML === Q10.correctAn || document.getElementById("option4").innerHTML === Q11.correctAn || document.getElementById("option4").innerHTML === Q12.correctAn || document.getElementById("option4").innerHTML === Q13.correctAn || document.getElementById("option4").innerHTML === Q14.correctAn || document.getElementById("option4").innerHTML === Q15.correctAn || document.getElementById("option4").innerHTML === Q16.correctAn){
+        correct();
+        i++;
+        correctCount++;
+        loadQuestion();
+    }
+    else {
+        wrong();
+        i++;
+        wrongCount++;
+        loadQuestion();
+    }
 }
 function play(){
-
 
 }
 function intro(){
@@ -110,7 +292,6 @@ function correct(){
     function closeCorrect(){
         document.getElementById("correct").style.display = "none";
     }
-    correctCount++;
     getCorrect();
     setTimeout(closeCorrect, 2000);
 }
@@ -123,13 +304,10 @@ function wrong(){
     }
     getWrong();
     setTimeout(closeWrong, 2000);
-    wrongCount++;
 }
 function reset(){
 
 }
 // create timer function
-// 
-
-
-intro();
+// intro();
+loadQuestion();
